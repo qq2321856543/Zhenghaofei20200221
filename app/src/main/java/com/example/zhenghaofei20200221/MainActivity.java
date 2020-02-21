9e5df5c65b34e7079cfe5a0fd39c56557c95faec
Administrator <2321856543@qq.com>
2020-02-21 13:33:36 +0800

    第一次创建项目

---
+++ app/src/main/java/com/example/zhenghaofei20200221/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.zhenghaofei20200221;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
